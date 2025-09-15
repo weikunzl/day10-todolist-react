5f78f396e7cf2850a9047870e6bd3849afcc7795
kunwei <kwei@thoughtworks.com>
2025-09-15 10:51:03 +0800

    feat: add default layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import './App.css';
-import {TodoList} from "./components/TodoList";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
+
+function DefaultLayout() {
+    return <>
+        <header>
+            <nav>
+                <ul>
+                    <li><NavLink to={'/'}>Home</NavLink></li>
+                    <li>Todo List</li>
+                    <li><NavLink to={'/about'}>About</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <h1>xxx</h1>
+            <Outlet></Outlet>
+        </main>
+        <footer>footer copyright</footer>
+    </>;
+}
+
+const routes = [
+    {
+        path: '/',
+        element: <DefaultLayout/>,
+        children: [{
+            path: '',
+            element: <h1>Home Page</h1>,
+        }, {
+            path: 'about',
+            element: <h1>About Us</h1>
+        }]
+
+    }
+]
+const router = createBrowserRouter(routes);
 
 function App() {
     return (
         <div className="App">
-            <TodoList/>
+            <RouterProvider router={router}></RouterProvider>
         </div>
     );
 }
