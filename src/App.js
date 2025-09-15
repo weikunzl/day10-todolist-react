05df00ff4ebbd790224a2030a8e01fa626d1eeb7
kunwei <kwei@thoughtworks.com>
2025-09-15 08:22:54 +0800

    refactor: refactor todo list layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import {useReducer} from "react";
 import './App.css';
-import {initialState, todoReducer} from "./reducers/todoReducer";
-import {TodoContext} from "./contexts/TodoContext";
 import {TodoList} from "./components/TodoList";
 
 function App() {
-    const [todos, dispatch] = useReducer(todoReducer, initialState);
     return (
         <div className="App">
-            <TodoContext.Provider value={{todos, dispatch}}>
-                <TodoList/>
-            </TodoContext.Provider>
+            <TodoList/>
         </div>
     );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,20 @@
 import './TodoList.css'
-import { TodoGroup } from "./TodoGroup";
-import { TodoGenerator } from "./TodoGenerator";
+import {TodoGroup} from "./TodoGroup";
+import {TodoGenerator} from "./TodoGenerator";
+import {useReducer} from "react";
+import {initialState, todoReducer} from "../reducers/todoReducer";
+import {TodoContext} from "../contexts/TodoContext";
 
 export const TodoList = () => {
+    const [todos, dispatch] = useReducer(todoReducer, initialState);
+
     return (
         <div className="todo-list">
-            <h1 className="todo-title">Todo List</h1>
-            <TodoGroup />
-            <TodoGenerator />
+            <TodoContext.Provider value={{todos, dispatch}}>
+                <h1 className="todo-title">Todo List</h1>
+                <TodoGroup/>
+                <TodoGenerator/>
+            </TodoContext.Provider>
         </div>
     );
 }
