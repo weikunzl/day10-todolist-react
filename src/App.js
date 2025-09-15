02c7c98b7c7b67cdd3e7902447f82736ff81803b
kunwei <kwei@thoughtworks.com>
2025-09-15 11:12:05 +0800

    feat: add todo list route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
+import {TodoList} from "./components/TodoList";
+import {TodoGroup} from "./components/TodoGroup";
 
 function DefaultLayout() {
     return <>
@@ -7,26 +9,33 @@ function DefaultLayout() {
             <nav>
                 <ul>
                     <li><NavLink to={'/'}>Home</NavLink></li>
-                    <li>Todo List</li>
+                    <li><NavLink to={'/todos'}>Todo List</NavLink></li>
                     <li><NavLink to={'/about'}>About</NavLink></li>
                 </ul>
             </nav>
         </header>
         <main>
-            <h1>xxx</h1>
             <Outlet></Outlet>
         </main>
         <footer>footer copyright</footer>
     </>;
 }
 
+function ErrorPage() {
+    return <h1>Error Page</h1>;
+}
+
 const routes = [
     {
         path: '/',
         element: <DefaultLayout/>,
+        errorElement: <ErrorPage/>,
         children: [{
             path: '',
             element: <h1>Home Page</h1>,
+        }, {
+            path: 'todos',
+            element: <TodoList/>,
         }, {
             path: 'about',
             element: <h1>About Us</h1>
