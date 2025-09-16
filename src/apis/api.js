7593030746422576f2ecb29f97ec4d25c19cba91
kunwei <kwei@thoughtworks.com>
2025-09-16 08:38:05 +0800

    ci: add github workflow

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,11 +2,49 @@ import axios from "axios";
 import {message} from 'antd';
 
 const instance = axios.create({
-    baseURL: 'https://68c78c8b5d8d9f5147322265.mockapi.io/api',
+    // baseURL: 'https://68c78c8b5d8d9f5147322265.mockapi.io/a',
+    baseURL: 'http://localhost:8080/',
     headers: {
         'Content-Type': 'application/json'
     }
 });
+
+instance.interceptors.request.use(
+    (config) => {
+        // request configuration
+        console.log("request success", config)
+        config.metadata = {
+            startTime: Date.now()
+        }
+        return config;
+    },
+    (error) => {
+        // handle request error
+        return Promise.reject(error);
+    }
+);
+
+instance.interceptors.response.use(
+    (response) => {
+        // handle response
+        console.log("response success", response)
+        console.log('Api duration is' + (Date.now() - response.config.metadata.startTime) +'ms')
+        return response;
+    },
+    (error) => {
+        // handle response error
+        const {status, data} = error.response;
+        message.error(`response Error ${status} ${data}`)
+
+        if (status === 401) {
+            console.log(error.response)
+            // do something
+        }
+        return Promise.reject(error);
+    }
+);
+
+
 
 export const getTodos = async () => {
     const response = await instance.get('/todos')
