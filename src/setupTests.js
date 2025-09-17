6a70012b64c344ebdacd2da1b723ed193721c485
kunwei <kwei@thoughtworks.com>
2025-09-17 11:59:24 +0800

    feat: 更新 Jest 配置以支持 axios，修改测试用例以反映主页内容

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfill for TextEncoder and TextDecoder in Node.js test environment
+import { TextEncoder, TextDecoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
